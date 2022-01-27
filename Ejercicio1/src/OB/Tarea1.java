ea547eb6528f070eec75e7b194c4236ea2071f4e
ims28 <98530626+ims28@users.noreply.github.com>
2022-01-27 14:09:30 +0100

    Add files via upload
    Ejercicio1 de Java Básico

---
+++ Ejercicio1/src/OB/Tarea1.java
@@ -0,0 +1,31 @@
+package OB;
+
+public class Tarea1 {
+    public static void main(String[] args) {
+
+        /*Crea un proyecto de Java desde 0
+         *Dentro del proyecto crear un paquete. En el paquete crear una clase.
+         *Dentro de la clase crear el método main e imprimir todos los datos que se han visto en las sesiones.
+         */
+
+        int numero1 = 8;
+        System.out.println("El valor de numero1 es: " + numero1);
+
+        long numero2 = 12345678;
+        System.out.println("El valor de numero2 es: " + numero2);
+
+        float decimal1 = 45.6f;
+        System.out.println("El valor de decimal1 es: " + decimal1);
+
+        double decimal2 = 55.67;
+        System.out.println("El valor de decimal2 es: " + decimal2);
+
+        boolean variable = true;
+        System.out.println("El valor de variable es: " + variable);
+
+        String texto = "Hola mundo";
+        System.out.println("La variable texto contiene: " + texto);
+
+
+    }
+}
